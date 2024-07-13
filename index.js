af2348906f8cae767fdee334c6e39bbf3a0c3d25
Nazmul Hassan <nazmulnhb007@yahoo.com>
2024-07-13 10:15:01 +0600

    created a function to find multiples of a number

--- index.js
+++ index.js
@@ -4,3 +4,17 @@ console.log('Hi from Shejan-2');
 console.log('Hi from Shejan-3');
 console.log('Hi from Shejan-4');
 console.log('Hi from Shejan-5');
+
+// find multiples of a number
+const findMultiples = (number = 1) => {
+    let multiples = [];
+    for (let i = 1; i <= number; i++) {
+        if (number % i === 0) {
+            multiples.push(i)
+        }
+    }
+    return multiples;
+}
+
+console.log(findMultiples(28));
+console.log(findMultiples(8));
